1f30595b494d94277bd3407d546e1fd1154f394f
Dominic Volante <dominicvolante@gmail.com>
2021-01-25 16:51:06 -0500

    worked on landing page and Readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Cari's List
+
+Cari's List is a place for people to learn about products that are good for the environment, and how to dispose of them and their packaging.
+
+Cari's List allows users to view products and read about their disposal instructions. It also allows users to visit the website of the company that makes each product.
+
+There is also a list of services that offer monthly memberships.
+
+Users can sort by eco-friendly rating, from eco-friendly, to zero waste.
+
+Users can also filter from all products and services, to kitchen related items, to bathroom related products, to monthly services.
+
+## Tech and Links
+
+Cari's List is a Full Stack project using React hosted on Vercel, communicating with a NodeJS API with PostgreSQL database, hosted on Heroku.
+
+Live Link: https://caris-list.vercel.app/
+API repository: https://github.com/DominicVolante/caris-list-server
+
+###### Landing Page
+
+![Landing Page](/screenshots/landingPageImg.png)
+
+###### Home Page
+
+![unsorted home](/screenshots/home.png)
+
+###### Expanded Product Page
+
+![sorted home](/screenshots/expandedPage.png)
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,29 +1,48 @@
 import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import MyCarousel from "./MyCarousel";
-import imageOne from "../images/mohonk1.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowAltCircleLeft,
+  faSeedling,
+} from "@fortawesome/free-solid-svg-icons";
 
 class LandingPage extends Component {
   state = {};
   render() {
     return (
-      <div className="wrapper landing_bar">
-        <div className="landing_left ">
-          <h1>
-            Dont know how to be "eco-friendly?" <br /> Check Cari's List.
-          </h1>
-          <h2>
-            {" "}
-            Cari's list is a place where you can find eco-friendly <br />{" "}
-            options for the common things you use.{" "}
-          </h2>
-        </div>
-        <div className="center">
-          <Link to={{ pathname: `/home` }}>
-            <button className="form-button center">Get Started!</button>
-          </Link>
+      <div>
+        <div className="wrapper landing_bar">
+          <div className="landing_left ">
+            <h1>
+              Struggling to be "eco-friendly?" <br /> Check Cari's List.
+            </h1>
+            <h2>
+              {" "}
+              Cari's list is a place where you can find eco-friendly <br />{" "}
+              options for the common things you use.{" "}
+            </h2>
+          </div>
+          <ul className="landing_right">
+            <h3>Everything on Cari's List is eco-friendly.</h3>
+            <li>
+              Eco-friendly:{" "}
+              <FontAwesomeIcon icon={faSeedling} size="1x" alt="1" />
+            </li>
+            <li>
+              Very eco-friendly:{" "}
+              <FontAwesomeIcon icon={faSeedling} size="1x" alt="1" />{" "}
+              <FontAwesomeIcon icon={faSeedling} size="1x" alt="1" />
+            </li>
+            <li>
+              Zero waste:{" "}
+              <FontAwesomeIcon icon={faSeedling} size="1x" alt="1" />{" "}
+              <FontAwesomeIcon icon={faSeedling} size="1x" alt="1" />{" "}
+              <FontAwesomeIcon icon={faSeedling} size="1x" alt="1" />
+            </li>
+            <Link to={{ pathname: `/home` }}>
+              <button className="form-button center">Get Started!</button>
+            </Link>
+          </ul>
         </div>
       </div>
     );
